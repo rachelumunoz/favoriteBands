fe9846c8511f57ea320af827d84634a227a8f0bd
rachelmunoz <rachelumunoz>
2017-10-14 20:48:48 -0700

    Adds ArtistActivity, implements onClickListener in ArtistHolder to navigate to ArtistActivity

---
+++ app/src/main/java/io/rachelmunoz/favoritebands/ArtistDetail/ArtistActivity.java
@@ -0,0 +1,15 @@
+package io.rachelmunoz.favoritebands.ArtistDetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.rachelmunoz.favoritebands.R;
+
+public class ArtistActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_artist);
+	}
+}
--- app/src/main/java/io/rachelmunoz/favoritebands/RecyclerAdapter.java
+++ app/src/main/java/io/rachelmunoz/favoritebands/RecyclerAdapter.java
@@ -1,18 +1,30 @@
 package io.rachelmunoz.favoritebands;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
+import io.rachelmunoz.favoritebands.ArtistDetail.ArtistActivity;
+import io.rachelmunoz.favoritebands.REST.ApiInterface;
+import io.rachelmunoz.favoritebands.REST.ArtistClient;
+
+
+
+
 /**
  * Created by rachelmunoz on 10/13/17.
  */
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ArtistHolder> {
+	private final String TAG = "RecyclerAdapter";
+
 	private List<Artist> mArtists;
 
 	public RecyclerAdapter(List<Artist> artists) {
@@ -22,6 +34,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Artist
 	@Override
 	public ArtistHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+
 		return new ArtistHolder(layoutInflater, parent);
 	}
 
@@ -30,8 +43,6 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Artist
 		Artist artist = mArtists.get(position);
 		holder.bind(artist);
 	}
-
-
 
 	@Override
 	public int getItemCount() {
@@ -42,7 +53,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Artist
 		mArtists = artists;
 	}
 
-	public class ArtistHolder extends RecyclerView.ViewHolder {
+	public class ArtistHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 		private ImageView mArtistImage;
 		private TextView mArtistName;
 		private ImageView mFavoriteIcon;
@@ -50,15 +61,26 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Artist
 
 		public ArtistHolder(LayoutInflater inflater, ViewGroup parent){
 			super(inflater.inflate(R.layout.list_item_artist, parent, false));
+			itemView.setOnClickListener(this);
 
 			mArtistImage = (ImageView) itemView.findViewById(R.id.artist_image);
 			mArtistName = (TextView) itemView.findViewById(R.id.artist_name);
 			mFavoriteIcon = (ImageView) itemView.findViewById(R.id.favorite_icon);
+
 		}
 
 		public void bind(Artist artist) {
 			mArtist = artist;
-			mArtistName.setText(artist.getName());
+			mArtistName.setText(mArtist.getName());
+		}
+
+		@Override
+		public void onClick(View view) {
+			Context context = view.getContext();
+			Intent intent = new Intent(context, ArtistActivity.class);
+			context.startActivity(intent);
 		}
 	}
 }
+
+
